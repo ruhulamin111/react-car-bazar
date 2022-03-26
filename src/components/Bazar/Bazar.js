952b7852b672c7e37e94c0b88c11f2f1212175b0
ruhulamin111 <ruhuljuwel111@gmail.com>
2022-03-26 23:10:40 +0600

    all clear

--- src/components/Bazar/Bazar.js
+++ src/components/Bazar/Bazar.js
@@ -36,6 +36,13 @@ const Bazar = () => {
                 </div>
 
             </div>
+            <div className="theory">
+                <h4>Question: How does react work?</h4>
+                <p>Ans: React is implement of Virtual DOM tree that is representation of Javascript. When it need to update it find most efficient way to update the browser's DOM. Browser's DOM and React element are plain and it cheap to creat. React DOM takes care of updating DOM to match the React elements. That's reason javascript is so fast and work keeping a DOM tree in it speed to manupulated.  </p>
+                <h4>Question: What is the difference of Props Vs State?</h4>
+                <p>Ans: Props read only and state is write and read both. Props can be used with component and function where state is use only state components. Props can not be editable there state can be editable. Props pass data from one to another components where state pass data within the component only.  </p>
+
+            </div>
 
         </div>
     );
